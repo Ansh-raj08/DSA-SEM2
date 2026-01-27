b04f9d601fe1494fabe086f453b5d0750d59e3ca
absolute-one <raj.ansh0811@gmail.com>
2026-01-27 15:26:45 +0530

    First commit

---
+++ DSA.py
@@ -0,0 +1,32 @@
+# decimal = 0
+# binary = 1011
+# powr = 0 
+
+# while binary > 0:
+
+# arr = [1,2,3,4]
+# revarry = []
+
+# @ 27 JAN 
+
+import array as ar
+
+arrName = ar.array('i',[12,6,8,9,5])
+print(arrName)
+# operations
+
+# 1.Add
+# a. append
+
+arrName.append(10) #append function only takes one number at a time
+print(arrName)
+
+arrName.insert(0,-9)
+print(arrName)
+
+arrName.pop(2)
+print(arrName)
+
+arrName.reverse() #reverse func takes no arguments in it's bracket()
+print(arrName)
+
