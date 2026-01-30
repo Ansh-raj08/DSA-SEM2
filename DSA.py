d22a670b09002d528177c9497604438ca480160e
absolute-one <raj.ansh0811@gmail.com>
2026-01-30 20:35:36 +0530

    commit

--- DSA.py
+++ DSA.py
@@ -11,22 +11,84 @@
 
 import array as ar
 
-arrName = ar.array('i',[12,6,8,9,5])
-print(arrName)
+# arrName = ar.array('i',[12,6,8,9,5])
+# print(arrName)
 # operations
 
 # 1.Add
 # a. append
 
-arrName.append(10) #append function only takes one number at a time
-print(arrName)
+# arrName.append(10) #append function only takes one number at a time
+# print(arrName)
 
-arrName.insert(0,-9)
-print(arrName)
+# arrName.insert(0,-9)
+# print(arrName)
 
-arrName.pop(2)
-print(arrName)
+# arrName.pop(2)
+# print(arrName)
 
-arrName.reverse() #reverse func takes no arguments in it's bracket()
-print(arrName)
+# arrName.reverse() #reverse func takes no arguments in it's bracket()
+# print(arrName)
+
+# @ 28 JAN 11:05 AM
+
+arrayName = ar.array('i',[22,33,44,55])
+# print(arrayName)
+
+# 1. Add
+# a) append (value) --> add the values in last, T/C - O(1), O(N)
+# b) insert (index, value) --> inserts the value at index, T/C - O(1), O(n)
+
+# 2. Delete
+# 3. Remove()
+# 4. pop()
+
+arrayName.remove(22)
+arrayName.pop()
+# print(arrayName)
+
+arrayName[0]=40 # to update the array elements.
+arrayName[1]=50
+# print(arrayName)
+
+# for i in range(0, len(arrayName)):
+#     print(arrayName[i], end=" ")
+
+# write a program to get sum and product.
+
+# sum = 0
+# product = 1
+# for val in arrayName:
+#     sum += val
+#     product *= val
+
+# print(sum)
+# print(product)
+
+# count = 0
+# target = int(input("Enter target value: "))
+
+# for val in arrayName:
+#     if val == target:
+#         count += 1
+
+# print(count)
+
+inc = True
+dec = True
+for i in range (0, len(arrayName)-1):
+    if(arrayName[i]>arrayName[i+1]):
+        inc = False
+    if(arrayName[i]<arrayName[i+1]):
+        dec = False
+
+if(inc == True):
+    print("It's in increasing order.")
+
+elif(dec == True):
+    print("It's in descending order.")
+
+else:
+    print("Not Sorted.")
+
 
---
+++ DSA2.py
@@ -0,0 +1,35 @@
+# arr = [12,11,8,7,4]
+# max_num = arr[0]
+# for num in arr[1:]:
+#     if num > max_num:
+#         max_num = num
+# print(max_num)
+# arr.pop(0)
+# max2_num = arr[0]
+# for num2 in arr[1:]:
+#     if num > max2_num:
+#         max2_num = num2
+# print(max2_num)
+
+# arr = [12,55,76,34,65]
+# max1_num = max2_num = float('-inf')
+# for num in arr:
+#     if num > max1_num:
+#         max2_num = max1_num
+#         max1_num = num
+#     elif num > max2_num and num != max1_num:
+#         max2_num = num
+# print(f"first largest number: {max1_num} second largest number: {max2_num}")
+
+# Two Sum problem-
+
+arr = [0,2,3,6,3,7]
+target = 9
+
+for i in range(0,len(arr)):
+    first_num = arr[i]
+
+    for j in range(i,len(arr)):
+        second_num = arr[j]
+        if first_num + second_num == target:
+            print(i,j)
