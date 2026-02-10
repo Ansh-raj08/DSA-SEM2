055fef97c4d526c7b620c4ea876324782aa467f4
absolute-one <raj.ansh0811@gmail.com>
2026-02-10 15:09:13 +0530

    10 feb

---
+++ DSA3.py
@@ -0,0 +1,17 @@
+# Print alternate elements from the array
+
+# arr = [1,2,3,4,5,6,7,8]
+
+# def print_alt(arr):
+#     for i in range (0, len(arr), 2):
+#         print(arr[i], end= " ")
+# print_alt(arr)
+
+# Unique element 
+
+# from collections import Counter
+# arr = [1,2,1,3,3,4,5,6,5,6,4]
+# for val in arr:
+#     if arr.count(val) == 1:
+#         print(val)
+
